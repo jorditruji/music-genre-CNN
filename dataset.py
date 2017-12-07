5284e31cef5bef350f10b1a8f3b548e1ca01d2e0
jorditruji <jordimoreratrujillo@gmail.com>
2017-12-07 23:00:33 +0100

    rnn style

--- dataset.py
+++ dataset.py
@@ -58,6 +58,7 @@ class dataset:
             class_data = []
             for file in data_class:
                 audio=load_audio(file)
+                print (audio.shape)
                 audio=audio.reshape(6,44100)
                 for frame in range(audio.shape[0]):
 
