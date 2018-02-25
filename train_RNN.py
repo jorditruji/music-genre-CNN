b8cf569f23e0a500ce2119c9032cdef2a1f1494c
jorditruji <jordimoreratrujillo@gmail.com>
2018-02-25 12:49:49 +0100

    fixing rnn

--- train_RNN.py
+++ train_RNN.py
@@ -74,7 +74,7 @@ input_tensor = Input(shape=(1, 264600))
 print(input_tensor.shape)
 model = Sequential()
 model.add(LSTM(output_dim=50,
-    return_sequences=True, input_shape= input_tensor.shape))
+    return_sequences=True, input_shape= X_train.shape[-1]))
 
 model.add(Dense(10, activation='softmax'))
 
